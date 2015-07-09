675ef207d75744e03e4017e8b3dd1d841d038c4b
niwakazoider <pethitto@gmail.com>
2015-07-10 03:13:25 +0900

    Update Sound.cs

--- Sound.cs
+++ Sound.cs
@@ -8,9 +8,9 @@ namespace gtktest
 		SoundPlayer player;
 		public Sound ()
 		{
-				player = new SoundPlayer();
-				player.SoundLocation = "sound.wav";
-				player.Load();
+			player = new SoundPlayer();
+			player.SoundLocation = "sound.wav";
+			player.Load();
 		}
 		public void play(){
 			if (player.IsLoadCompleted)
