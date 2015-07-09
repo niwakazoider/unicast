36a5dce80e3ec196d5177a6efe09d17cd56eebcd
niwakazoider <pethitto@gmail.com>
2015-07-10 03:07:54 +0900

    最初のコミット

---
+++ Sound.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Media;
+
+namespace gtktest
+{
+	public class Sound
+	{
+		SoundPlayer player;
+		public Sound ()
+		{
+				player = new SoundPlayer();
+				player.SoundLocation = "sound.wav";
+				player.Load();
+		}
+		public void play(){
+			if (player.IsLoadCompleted)
+			{
+				player.Play();
+			}
+		}
+	}
+}
+
